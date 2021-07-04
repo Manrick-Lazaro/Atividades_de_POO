63223c4047f67e0a24c5f3b9985a758e240b1b3f
manrick lazaro <77447723+Manrick-Lazaro@users.noreply.github.com>
2021-07-04 02:11:21 +0000

    03/07/2021 23:11

---
+++ POO - Lista 03/Ex-02.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Frete
+{
+  private double distancia, peso;
+
+  public Frete (double distancia, double peso)
+  {
+    if (distancia > 0) this.distancia = distancia;
+    if (peso > 0) this.peso = peso;
+  }
+
+  public void SetDistancia (double distancia)
+  {
+    if (distancia > 0) this.distancia = distancia;
+  }
+  public void SetPeso (double peso)
+  {
+    if (peso > 0) this.peso = peso;
+  }
+
+  public double GetDistancia ()
+  {
+    return distancia;
+  }
+  public double GetPeso ()
+  {
+    return peso;
+  }
+
+  public double CalcFrete (double distancia, double peso)
+  {
+    // peso * 0.01 * distancia == Frete
+    return ((peso * 0.01) * distancia);
+  }
+
+  public override string ToString ()
+  {
+    return $"Distancia: {distancia}\nPeso: {peso}";
+  }
+}
