cb2420acd4765dafe595c1ab05e45aa8c3eea56b
manrick lazaro <77447723+Manrick-Lazaro@users.noreply.github.com>
2021-07-04 01:49:29 +0000

    03/07/2021 22:49

---
+++ POO - Lista 03/Ex-01.cs
@@ -0,0 +1,44 @@
+using System;
+
+class Retangulo 
+{
+  private double b, h;
+
+  public Retangulo (double b, double h)
+  {
+    if (b > 0) this.b = b;
+    if (h > 0) this.h = h;
+  }
+
+  public void SetBase (double b)
+  {
+    if (b > 0) this.b = b;
+  }
+  public void SetAltura (double h)
+  {
+    if (h > 0) this.h = h;
+  }
+
+  public double GetBase()
+  {
+    return b;
+  }
+  public double GetAltura()
+  {
+    return h;
+  }
+
+  public double CalcArea (double b, double h)
+  {
+    return b * h;
+  }
+  public double CalcDiagonal (double b, double h)
+  {
+    return ((b * h) / 2);
+  }
+
+  public override string ToString ()
+  {
+    return $"Base: {b} \nAltura: {h}";
+  }
+}
